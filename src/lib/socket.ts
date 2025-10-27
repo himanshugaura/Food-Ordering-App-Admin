86c32f9486d2dac1a264393a1a86f1c201e74362
Himanshu Gaura <himanshu32505@gmail.com>
2025-10-27 16:09:45 +0530

    url update

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -9,7 +9,7 @@ interface ClientToServerEvents {
   joinRoom: (room: string) => void;
 }
 
-const SOCKET_URL =   "http://localhost:3030";
+const SOCKET_URL =  import.meta.env.VITE_BASE_URL as string;
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(SOCKET_URL, {
   autoConnect: false,
